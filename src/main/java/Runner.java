3f74bcabfe3427bd597796e289f9fa1b779aa03e
sakusanpuwan <sakusanpuwan98@gmail.com>
2022-09-09 12:15:08 +0100

    pull request

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("develop");
+        System.out.println("make a pull");
     }
 
 }
